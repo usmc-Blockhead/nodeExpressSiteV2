eeb98b7ec396605326610da908fb2da1c211e204
Christa <jaysplaypen@gmail.com>
2023-05-11 23:40:23 -0700

    Express Session Part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 //Routes
 var indexRouter = require("./routes/index");
@@ -36,11 +38,82 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+//Basic Authentication
+// app.use(cookieParser());
+
+//Cookies
+// app.use(cookieParser('12345-67890-09876-54321'));
 
 //Basic Authentication
+// function auth(req, res, next) {
+//     console.log(req.headers);
+//     const authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         const err = new Error('You are not authenticated!');
+//         res.setHeader('WWW-Authenticate', 'Basic');
+//         err.status = 401;
+//         return next(err);
+//     }
+//     const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+//     const user = auth[0];
+//     const pass = auth[1];
+//     if (user === 'admin' && pass === 'password') {
+//         return next(); // authorized
+//     } else {
+//         const err = new Error('You are not authenticated!');
+//         res.setHeader('WWW-Authenticate', 'Basic');      
+//         err.status = 401;
+//         return next(err);
+//     }
+// }
+
+//Cookies
+// function auth(req, res, next) {
+//     if (!req.signedCookies.user) {
+//         const authHeader = req.headers.authorization;
+//         if (!authHeader) {
+//             const err = new Error('You are not authenticated!');
+//             res.setHeader('WWW-Authenticate', 'Basic');
+//             err.status = 401;
+//             return next(err);
+//         }
+
+//         const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+//         const user = auth[0];
+//         const pass = auth[1];
+//         if (user === 'admin' && pass === 'password') {
+//             res.cookie('user', 'admin', {signed: true});
+//             return next(); // authorized
+//         } else {
+//             const err = new Error('You are not authenticated!');
+//             res.setHeader('WWW-Authenticate', 'Basic');
+//             err.status = 401;
+//             return next(err);
+//         }
+//     } else {
+//         if (req.signedCookies.user === 'admin') {
+//             return next();
+//         } else {
+//             const err = new Error('You are not authenticated!');
+//             err.status = 401;
+//             return next(err);
+//         }
+//     }
+// }
+
+//Express-Session
+app.use(session({
+    name: 'session-id',
+    secret: '12345-67890-09876-54321',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}));
+
 function auth(req, res, next) {
-    if (!req.signedCookies.user) {
+    console.log(req.session);
+
+    if (!req.session.user) {
         const authHeader = req.headers.authorization;
         if (!authHeader) {
             const err = new Error('You are not authenticated!');
@@ -53,7 +126,7 @@ function auth(req, res, next) {
         const user = auth[0];
         const pass = auth[1];
         if (user === 'admin' && pass === 'password') {
-            res.cookie('user', 'admin', {signed: true});
+            req.session.user = 'admin';
             return next(); // authorized
         } else {
             const err = new Error('You are not authenticated!');
@@ -62,7 +135,7 @@ function auth(req, res, next) {
             return next(err);
         }
     } else {
-        if (req.signedCookies.user === 'admin') {
+        if (req.session.user === 'admin') {
             return next();
         } else {
             const err = new Error('You are not authenticated!');
@@ -71,6 +144,7 @@ function auth(req, res, next) {
         }
     }
 }
+
 app.use(auth);
 
 //Static Pages
